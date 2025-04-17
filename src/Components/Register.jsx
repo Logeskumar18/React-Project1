0bd5d94bf4d0d533a67bd48b61591bee6ef711e4
Loges <kumarloges329@gmail.com>
2025-04-17 15:13:08 +0530

    added

--- src/Components/Register.jsx
+++ src/Components/Register.jsx
@@ -1,7 +1,59 @@
 import React from 'react'
+import { Button1 } from './Button1';
+import Button2 from './Button2';
+import img1 from 'H:/React/project1/src/assets/Background.png';
 
 export const Register = () => {
-  return (
-    <h1>Register</h1>
-  )
+    return (
+        <div className="container  d-flex justify-content-center" style={{
+            backgroundImage: `url(${img1})`,
+            backgroundSize: 'cover',
+            backgroundPosition: 'center',
+        }}>
+            <div className=" p-5 rounded-4 shadow-lg" style={{ width: '400px' }}>
+                <h3 className="text-center animateHeading my-4 fs-2">Create an account</h3>
+
+                <div className="mb-3">
+                    <label htmlFor="name" className="form-label" style={{ color: '#7b8bab' }}>Name</label>
+                    <input type="text" className="form-control" style={{ backgroundColor: "#0c1725" }} id="name" placeholder="Enter your Name" />
+                </div>
+
+                <div className="mb-3">
+                    <label htmlFor="Companyname" className="form-label" style={{ color: '#7b8bab' }}>Company Name</label>
+                    <input type="text" className="form-control" style={{ backgroundColor: "#0c1725" }} id="Companyname" placeholder="Enter your Name" />
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="e-mail" className="form-label" style={{ color: '#7b8bab' }}>Work Email</label>
+                    <input type="email" className="form-control" style={{ backgroundColor: "#0c1725" }} id="e-mail" placeholder="Enter your Work email" />
+                </div>
+                <div className="mb-3">
+                    <div className="d-flex justify-content-between">
+                        <label htmlFor="password" className="form-label" style={{ color: '#7b8bab' }}>Password</label>
+
+                    </div>
+                    <input type="password" style={{ backgroundColor: "#0c1725" }} className="form-control" id="password" placeholder="Enter your password" />
+                </div>
+
+                <div className="">
+                    <Button1 name="Register" />
+                </div>
+
+                <div className="d-flex align-items-center my-4">
+                    <hr className="flex-grow-1" />
+                    <span className="mx-2 text-muted">or</span>
+                    <hr className="flex-grow-1" />
+                </div>
+
+                <div className="">
+                    <Button1 name="Sign In with Google" />
+                </div>
+
+                <div className='d-flex mt-4 ms-3 '> 
+                        <p>Already have an account?</p><span href="">Sign In</span>
+                    </div>
+
+            </div>
+
+        </div>
+    )
 }
--- src/Components/Signin.jsx
+++ src/Components/Signin.jsx
@@ -1,30 +1,56 @@
-import React from 'react'
+import React from 'react';
 import '../index.css';
 import Button2 from './Button2';
+import img1 from 'H:/React/project1/src/assets/Background.png';
 import { Button1 } from './Button1';
 
 export const Signin = () => {
     return (
         <>
-            <div className="container my-5">
-                <h3 className='text-center animateHeading fs-2'>Welcome Back</h3>
-                <div>
-                    <div className='mt-3'><label htmlFor="e-mail">Email
-                    </label>
-                        <input type="email" className='form-control w-25' name="e-mail" /></div>
-                    <div className='mt-3'><label htmlFor="password">password</label> <a>Forgot?</a>
-                        <input type='password' className='form-control w-25' name='password' /></div>
-                    <div className="mt-3"><Button2 name="Sign in" /></div>
-                    <div class="d-flex align-items-center my-4">
-                        <hr class="flex-grow-1" />
-                            <span class="mx-2 text-muted">or</span>
-                            <hr class="flex-grow-1"/>
-                            </div>
-                            <div>
-                                <Button1 name="Sign in with Google" />
-                            </div>
+            <div className="container  d-flex justify-content-center" style={{
+                backgroundImage: `url(${img1})`,
+                backgroundSize: 'cover',
+                backgroundPosition: 'center',
+            }}>
+                <div className=" p-5 rounded-4 shadow-lg" style={{ width: '400px' }}>
+                    <h3 className="text-center animateHeading my-4 fs-2">Welcome Back</h3>
+
+
+                    <div className="mb-3">
+                        <label htmlFor="e-mail" className="form-label" style={{color: '#7b8bab'}}>Email</label>
+                        <input type="email" className="form-control" style={{ backgroundColor: "#0c1725" }} id="e-mail" placeholder="Enter your email" />
+                    </div>
+
+                    {/* Password Field */}
+                    <div className="mb-3">
+                        <div className="d-flex justify-content-between">
+                            <label htmlFor="password" className="form-label" style={{color: '#7b8bab'}}>Password</label>
+                            <a href="#" className="text-decoration-none small text-muted">Forgot?</a>
+                        </div>
+                        <input type="password" style={{ backgroundColor: "#0c1725" }} className="form-control" id="password" placeholder="Enter your password" />
+                    </div>
+
+                    {/* Sign In Button */}
+                    <div className="mt-4 d-flex justify-content-center ">
+                        <Button2  name="Sign In" />
+                    </div>
+
+                    {/* Divider */}
+                    <div className="d-flex align-items-center my-4">
+                        <hr className="flex-grow-1" />
+                        <span className="mx-2 text-muted">or</span>
+                        <hr className="flex-grow-1" />
+                    </div>
+
+                    {/* Google Sign In Button */}
+                    <div className="">
+                        <Button1 name="Sign In with Google" />
+                    </div>
+                    <div className='d-flex mt-4 ms-3 '> 
+                        <p>Don't you have an account?</p><span href="">Sign Up</span>
                     </div>
                 </div>
-            </>
-            )
+            </div>
+        </>
+    );
 }
